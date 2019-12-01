a6073d42ccace451c1bc8a14c271e6c97594a2fc
Kishan Siwakoti <kishan.siwako@gmail.com>
2019-12-01 14:45:32 -0600

    Enabled Migrations/ New Project Created

---
+++ SportHub/SportHub/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+namespace SportHub.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<SportHub.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(SportHub.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
